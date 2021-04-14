4016e9126de21c02b0a1eea2e3cd107b88ccc921
sudoghut <sudospace@gmail.com>
2021-04-14 09:50:06 +0800

    change varible names

--- sims/compare_by_each_chars.py
+++ sims/compare_by_each_chars.py
@@ -1,12 +1,12 @@
-def compare(char_1, char_2, algorithm_var):
-    char_1_set = set(char_1)
-    char_1_set_len = len(char_1_set)
-    char_2_set = set(char_2)
-    char_2_set_len = len(char_2_set)
-    intersection_set = char_1_set.intersection(char_2_set)
+def compare(str_1, str_2, algorithm_var):
+    str_1_set = set(str_1)
+    str_1_set_len = len(str_1_set)
+    str_2_set = set(str_2)
+    str_2_set_len = len(str_2_set)
+    intersection_set = str_1_set.intersection(str_2_set)
     intersection_set_len = len(intersection_set)
-    if(char_1_set_len != 0 and char_2_set_len != 0):
-        max_rate = max(intersection_set_len/char_1_set_len, intersection_set_len/char_2_set_len)
+    if(str_1_set_len != 0 and str_2_set_len != 0):
+        max_rate = max(intersection_set_len/str_1_set_len, intersection_set_len/str_2_set_len)
     else:
         max_rate = 0
     return max_rate, [intersection_set_len, ";".join(intersection_set)]
