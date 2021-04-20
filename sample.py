94ce7a3be26b6ebf4edd8e533c09188d2b1c9ab3
sudoghut <sudospace@gmail.com>
2021-04-20 22:47:23 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,7 +1,9 @@
+'''Compare two strings by using each of their characters (Hongsu Wang)'''
 from sims import compare_by_each_chars
 output = compare_by_each_chars.compare("str1", "str2", [])
 print(output)
 
+'''Compare two strings by using each of substrings of candidate string (Linxu Wang)'''
 from sims import compare_by_each_substrings
 output = compare_by_each_substrings.compare("str1", "str12", ["str1"])
 print(output)
