50e53289715152d4a5746e20350304f1447c2f80
unknown <vicky_wanglx@163.com>
2021-04-20 18:05:32 +0800

    Add comparison function

--- sample.py
+++ sample.py
@@ -12,3 +12,18 @@
 from sims import compare_by_each_chars
 output = compare_by_each_chars.compare("str1", "str2", [])
 print(output)
+
+
+#  compare_by_each_substring - Linxu Wang
+#  Description:
+#    Compare two strings by using each of substrings of candidate string
+#  Reference link:
+#    None
+#  Input:
+#    The first two variables - the strings need to be compared - string
+#    The third varible - which string is the target string (the third variable must same as the one of the first two variables)  - string
+#  Output:
+#    The first varible - similarity degree - number
+from sims import compare_by_each_substrings
+output = compare_by_each_substrings.compare("str1", "str12", "str1")
+print(output)
